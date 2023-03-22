bfe07d1ab0cae1345b2694123095127bfe90c9e3
Muhammad Hakimi <hakimi.maula@digital-omnis.com>
2023-03-22 19:45:51 +0800

    updated readme

--- Controllers/admin-controller.js
+++ Controllers/admin-controller.js
@@ -1,11 +1,12 @@
-const Reviews = require("../models/reviews");
-const Events = require("../models/events");
-const User = require("../models/user");
+const Reviews = require("../Models/Reviews/reviews");
+const Events = require("../Models/Events/events");
+const User = require("../Models/Users/user");
+const News = require("../Models/News/news");
 const validateToken = require("../validation/validatetoken");
 const isEmpty = require("is-empty");
 
 // get reviews owned by one user, auth needed
-const getReviews = async (req, res, next) => {
+const getAdminData = async (req, res, next) => {
   try {
     const authHeaderValue = req?.headers["authorization"];
 
@@ -21,13 +22,41 @@ const getReviews = async (req, res, next) => {
     }
 
     const token = await authHeaderValue.replace("Bearer ", "");
-    const { error, id } = await validateToken(token);
+    const { error, id, isAdmin } = await validateToken(token);
+
+    if (!isAdmin) {
+      return res.status(422).json({
+        success: false,
+        message: "Only admins allowed",
+      });
+    }
+
+    const reviews = await Reviews.find({})
+      .populate({ path: "user", select: "username" })
+      .populate({ path: "event", select: "eventName" })
+      .select("review rating user event");
+
+    const events = await Events.find({})
+      .populate({ path: "reviews", select: "review rating user" })
+      .select("eventName description status");
+
+    const newsItems = await News.find();
 
     return res.status(200).json({
       status: "success",
-      data: "test",
+      data: {
+        reviews: reviews.map((review) => ({
+          review: review.review,
+          rating: review.rating,
+          user: review.user.username,
+          eventName: review.event.eventName,
+        })),
+        events,
+        newsItems,
+      },
     });
   } catch (error) {
+    console.log("----error", error);
     return res.status(422).json({
       status: "error",
       message: error?.errors?.event?.message || "An unexpected error occured",
@@ -36,47 +65,47 @@ const getReviews = async (req, res, next) => {
 };
 
 // add reviews, auth needed
-const addNotes = async (req, res, next) => {
-  try {
-    const authHeaderValue = req.headers && req.headers.authorization;
-    if (
-      !authHeaderValue ||
-      authHeaderValue === "undefined" ||
-      authHeaderValue === ""
-    ) {
-      return res.status(422).json({
-        success: false,
-        message: "You are required to login to add reviews",
-      });
-    }
-    const token = await authHeaderValue.replace("Bearer ", "");
-    const { error, id } = await validateToken(token);
-    // const { eventName, review, rating, event } = req.body;
-    const createdNotes = new Notes({
-      review,
-      rating,
-      event,
-      user: id,
-    });
+// const addNotes = async (req, res, next) => {
+//   try {
+//     const authHeaderValue = req.headers && req.headers.authorization;
+//     if (
+//       !authHeaderValue ||
+//       authHeaderValue === "undefined" ||
+//       authHeaderValue === ""
+//     ) {
+//       return res.status(422).json({
+//         success: false,
+//         message: "You are required to login to add reviews",
+//       });
+//     }
+//     const token = await authHeaderValue.replace("Bearer ", "");
+//     const { error, id } = await validateToken(token);
+//     // const { eventName, review, rating, event } = req.body;
+//     const createdNotes = new Notes({
+//       review,
+//       rating,
+//       event,
+//       user: id,
+//     });
 
-    // await createdNotes.save();
+//     // await createdNotes.save();
 
-    // await User.updateOne(
-    //   { _id: event },
-    //   { $push: { reviews: createdNotes._id } }
-    // );
+//     // await User.updateOne(
+//     //   { _id: event },
+//     //   { $push: { reviews: createdNotes._id } }
+//     // );
 
-    return res.status(200).json({
-      status: "success",
-    });
-  } catch (error) {
-    console.log("---error", error);
-    return res.status(422).json({
-      status: "error",
-      message: error?.errors?.event?.message || "An unexpected error occured",
-    });
-  }
-};
+//     return res.status(200).json({
+//       status: "success",
+//     });
+//   } catch (error) {
+//     console.log("---error", error);
+//     return res.status(422).json({
+//       status: "error",
+//       message: error?.errors?.event?.message || "An unexpected error occured",
+//     });
+//   }
+// };
 
-exports.addNotes = addNotes;
-exports.getReviews = getReviews;
+// exports.addNotes = addNotes;
+exports.getAdminData = getAdminData;
--- readme.md
+++ readme.md
@@ -17,3 +17,14 @@ Changelog v0.1.2 21/3/2023
 2. Add dummy data and route for events
 3. Add validation for checking auth token with jwt
 4. Add route for adding reviews while checking token with secret key
+
+Changelog v0.1.3 22/3/2023
+
+1. Fix and add review ID's to events schema array for easy indexing and fixed events name
+2. Added get event route
+3. Standardize several error handling
+4. Added route for admins and added isAdmin to jwt token
+5. Moved schema to different folders to avoild conflict
+6. Added isadmin and retrieve from token
+7. Add status publish to news and events and only return published content
+8. Fix file names as previously changed Models folder
